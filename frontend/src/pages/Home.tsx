776eb384dc0dce9669cc8d4b8f896a4c596a2d44
badbeardd <imsuraj90@gmail.com>
2025-05-06 12:24:50 +0530

    update 14 - frontend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 // frontend/src/pages/Home.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import OAuthButtons from '../components/OAuthButtons';
 
